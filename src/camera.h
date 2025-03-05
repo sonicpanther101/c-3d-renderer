2636f829f767a768d3a6573f4fd243c8b320321c
sonicpanther101 <sonicpanther101@gmail.com>
2025-03-05 21:34:58 +1300

    FPS camera

--- src/camera.h
+++ src/camera.h
@@ -71,13 +71,13 @@ public:
     {
         float velocity = MovementSpeed * deltaTime;
         if (direction == FORWARD)
-            Position += Front * velocity;
+            Position += Front * glm::vec3(1,0,1) * velocity;
         if (direction == BACKWARD)
-            Position -= Front * velocity;
+            Position -= Front * glm::vec3(1,0,1) * velocity;
         if (direction == LEFT)
-            Position -= Right * velocity;
+            Position -= Right * glm::vec3(1,0,1) * velocity;
         if (direction == RIGHT)
-            Position += Right * velocity;
+            Position += Right * glm::vec3(1,0,1) * velocity;
     }
 
     // processes input received from a mouse input system. Expects the offset value in both the x and y direction.
